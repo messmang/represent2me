a16891007bcfba98048ccea5e51c98a12ecec117
Jinmin Seong <d.messmang.b@gmail.com>
2024-04-25 02:22:10 +0900

    build

---
+++ precache-manifest.45393738788745eaa371d2264181079d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e97ed3a30657ca9a8ba73d9ef8ce4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdab1863492063ea0d22",
+    "url": "/static/css/main.4c9b6e3f.chunk.css"
+  },
+  {
+    "revision": "841688678df7c5a805f6",
+    "url": "/static/js/2.cf5b00f2.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "bdab1863492063ea0d22",
+    "url": "/static/js/main.7f742796.chunk.js"
+  },
+  {
+    "revision": "a50475a1f486eef27c5a",
+    "url": "/static/js/runtime~main.4414e1f1.js"
+  }
+]);
