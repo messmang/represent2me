55c32e7236007565a62842c128522d2c0c0b3e40
Jinmin Seong <d.messmang.b@gmail.com>
2024-04-29 00:27:32 +0900

    Fix build failure
    set base homepage

---
+++ precache-manifest.f169a0813e7ed81591a6d097f277cfe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23eead22a95f796ec3a46e8fd637280",
+    "url": "/represent2me/index.html"
+  },
+  {
+    "revision": "4f8cd76e1f65f649bbb6",
+    "url": "/represent2me/static/css/main.4c9b6e3f.chunk.css"
+  },
+  {
+    "revision": "44c596d5227cafd43bfd",
+    "url": "/represent2me/static/js/2.e77be6f7.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/represent2me/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "4f8cd76e1f65f649bbb6",
+    "url": "/represent2me/static/js/main.df4e1f33.chunk.js"
+  },
+  {
+    "revision": "be9fc882abb60267fda0",
+    "url": "/represent2me/static/js/runtime~main.4f94c631.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45393738788745eaa371d2264181079d.js"
+  "/represent2me/precache-manifest.f169a0813e7ed81591a6d097f277cfe7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/represent2me/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
